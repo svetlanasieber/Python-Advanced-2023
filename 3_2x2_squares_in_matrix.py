a3637ff4dab091d9f1b9821e2e38ddebc1796223
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-10-31 23:40:30 +0100

    Update 3_2x2_squares_in_matrix.py

--- 3_2x2_squares_in_matrix.py
+++ 3_2x2_squares_in_matrix.py
@@ -1,15 +1,3 @@
-# rows, columns = (int(x) for x in input().split())
-# matrix = [input().split() for i in range(rows)]
-# print(matrix)
-# counter = 0
-# for row in range(len(matrix)):
-#     for col in range(len(matrix[row])):
-#         if matrix[row][col] == matrix[row][col + 1] == matrix[row + 1][col] == matrix[row + 1][col + 1]:
-#             counter += 1
-#
-
-
-
 rows, cols = [int(x) for x in input().split()]
 
 matrix = [[x for x in input().split()] for x in range(rows)]
